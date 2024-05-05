6b4872d500ae6c6b931fd495fd129a1c8a39f1ca
Marcel Kaiser <mk@freeshell.de>
2024-05-05 15:37:55 +0200

    Change formatting license header.

--- lib/libdsbexec.c
+++ lib/libdsbexec.c
@@ -1,354 +1,291 @@
 /*-
- * Copyright (c) 2018 Marcel Kaiser. All rights reserved.
+ * Copyright (c) 2024 Marcel Kaiser. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * SPDX-License-Identifier: BSD-2-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <err.h>
 #include <errno.h>
 #define _WITH_GETLINE
-#include <stdio.h>
 #include <ctype.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#include <fcntl.h>
+#include <libutil.h>
+#include <limits.h>
+#include <paths.h>
+#include <pwd.h>
+#include <signal.h>
 #include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <pwd.h>
-#include <limits.h>
-#include <libutil.h>
 #include <time.h>
-#include <paths.h>
 #include <unistd.h>
-#include <signal.h>
-#include <fcntl.h>
 
 #include "libdsbexec.h"
 
-#define ERRBUFSZ	1024
-#define MAXARGS		256
-#define MAXHISTSIZE	25
-#define FATAL_SYSERR	(DSBEXEC_ERR_SYS | DSBEXEC_ERR_FATAL)
-#define PATH_HISTORY	".dsbexec.history"
-#define PATH_FIFO	"." PROGRAM ".lock"
+#define ERRBUFSZ 1024
+#define MAXARGS 256
+#define MAXHISTSIZE 25
+#define FATAL_SYSERR (DSBEXEC_ERR_SYS | DSBEXEC_ERR_FATAL)
+#define PATH_HISTORY ".dsbexec.history"
+#define PATH_FIFO "." PROGRAM ".lock"
 
+#define ERROR(ret, error, prepend, fmt, ...)       \
+  do {                                             \
+    set_error(error, prepend, fmt, ##__VA_ARGS__); \
+    return (ret);                                  \
+  } while (0)
 
-#define ERROR(ret, error, prepend, fmt, ...) do { \
-	set_error(error, prepend, fmt, ##__VA_ARGS__); \
-	return (ret); \
-} while (0)
-
-static int  strtoargv(const char *, char **, size_t, size_t *);
+static int strtoargv(const char *, char **, size_t, size_t *);
 static void set_error(int, bool, const char *, ...);
 
-static int    _error;
-static char   errmsg[ERRBUFSZ];
-static char   *history[MAXHISTSIZE];
-static char   path_fifo[PATH_MAX];
+static int _error;
+static char errmsg[ERRBUFSZ];
+static char *history[MAXHISTSIZE];
+static char path_fifo[PATH_MAX];
 static size_t histsize;
 
-const char *
-dsbexec_strerror()
-{
-	return (errmsg);
+const char *dsbexec_strerror() { return (errmsg); }
+
+int dsbexec_error() { return (_error); }
+
+int dsbexec_exec(bool sudo, const char *cmd, const char *msgstr) {
+  int eflags;
+  char *argv[MAXARGS];
+  size_t i, hs, argc;
+
+  if (histsize == 0) {
+    if (dsbexec_read_history(&hs) == NULL && _error != 0)
+      ERROR(-1, 0, true, "dsbexec_read_history()");
+  }
+  if (sudo) {
+    i = 1;
+    argv[0] = PATH_DSBSU;
+    if (msgstr != NULL && *msgstr != '\0') {
+      i += 2;
+      argv[1] = "-m";
+      argv[2] = (char *)msgstr;
+    }
+  } else
+    i = 0;
+  if (dsbexec_add_to_history(cmd) == -1) return (-1);
+  if (strtoargv(cmd, argv + i, MAXARGS - i - 1, &argc) == -1) return (-1);
+  argv[argc + i] = NULL;
+  if (dsbexec_write_history() == -1) return (-1);
+  execvp(argv[0], argv);
+  eflags = DSBEXEC_ERR_SYS | DSBEXEC_EEXECCMD;
+  if (errno == ENOENT) eflags |= DSBEXEC_ENOENT;
+  set_error(eflags, false, "execvp(%s)", argv[i]);
+  for (; i < argc; i++) free(argv[i]);
+  return (-1);
 }
 
-int
-dsbexec_error()
-{
-	return (_error);
+char **dsbexec_read_history(size_t *size) {
+  FILE *fp;
+  char *line, histpath[PATH_MAX];
+  size_t linecap;
+  struct passwd *pw;
+
+  _error = 0;
+  if ((pw = getpwuid(getuid())) == NULL)
+    ERROR(NULL, FATAL_SYSERR, false, "getpwuid()");
+  (void)snprintf(histpath, sizeof(histpath), "%s/%s", pw->pw_dir, PATH_HISTORY);
+  endpwent();
+  if ((fp = fopen(histpath, "r")) == NULL) {
+    if (errno != ENOENT)
+      ERROR(NULL, FATAL_SYSERR, false, "fopen(%s)", histpath);
+    return (NULL);
+  }
+  histsize = linecap = 0;
+  line = NULL;
+  while (getline(&line, &linecap, fp) > 0 && histsize < MAXHISTSIZE) {
+    if ((history[histsize++] = strdup(line)) == NULL)
+      ERROR(NULL, FATAL_SYSERR, false, "strdup()");
+    (void)strtok(history[histsize - 1], "\n\r");
+  }
+  free(line);
+  fclose(fp);
+
+  *size = histsize;
+
+  return (history);
 }
 
-int
-dsbexec_exec(bool sudo, const char *cmd, const char *msgstr)
-{
-	int    eflags;
-	char   *argv[MAXARGS];
-	size_t i, hs, argc;
-	
-	if (histsize == 0) {
-		if (dsbexec_read_history(&hs) == NULL && _error != 0)
-			ERROR(-1, 0, true, "dsbexec_read_history()");
-	}
-	if (sudo) {
-		i = 1;
-		argv[0] = PATH_DSBSU;
-		if (msgstr != NULL && *msgstr != '\0') {
-			i += 2;
-			argv[1] = "-m";
-			argv[2] = (char *)msgstr;
-		}
-	} else
-		i = 0;
-	if (dsbexec_add_to_history(cmd) == -1)
-		return (-1);
-	if (strtoargv(cmd, argv + i, MAXARGS - i - 1, &argc) == -1)
-		return (-1);
-	argv[argc + i] = NULL;
-	if (dsbexec_write_history() == -1)
-		return (-1);
-	execvp(argv[0], argv);
-	eflags = DSBEXEC_ERR_SYS | DSBEXEC_EEXECCMD;
-	if (errno == ENOENT)
-		eflags |= DSBEXEC_ENOENT;
-	set_error(eflags, false, "execvp(%s)", argv[i]);
-	for (; i < argc; i++)
-		free(argv[i]);
-	return (-1);
+int dsbexec_add_to_history(const char *cmd) {
+  size_t i, n;
+
+  _error = 0;
+  if (*cmd == '\0') return (0);
+  if (histsize >= MAXHISTSIZE) {
+    histsize = MAXHISTSIZE;
+    free(history[histsize - 1]);
+    n = histsize - 1;
+  } else
+    n = histsize++;
+  for (i = n; i >= 1; i--) history[i] = history[i - 1];
+  if ((history[0] = strdup(cmd)) == NULL)
+    ERROR(-1, FATAL_SYSERR, false, "strdup()");
+  (void)strtok(history[0], "\n\r");
+
+  return (0);
 }
 
-char **
-dsbexec_read_history(size_t *size)
-{
-	FILE	      *fp;
-	char	      *line, histpath[PATH_MAX];
-	size_t	      linecap;
-	struct passwd *pw;
+int dsbexec_write_history() {
+  int fd;
+  char histpath[PATH_MAX], tmpath[PATH_MAX];
+  FILE *tmp;
+  size_t i;
+  struct passwd *pw;
 
-	_error = 0;
-	if ((pw = getpwuid(getuid())) == NULL)
-		ERROR(NULL, FATAL_SYSERR, false, "getpwuid()");
-	(void)snprintf(histpath, sizeof(histpath), "%s/%s", pw->pw_dir,
-	    PATH_HISTORY);
-	endpwent();
-	if ((fp = fopen(histpath, "r")) == NULL) {
-		if (errno != ENOENT)
-			ERROR(NULL, FATAL_SYSERR, false, "fopen(%s)", histpath);
-		return (NULL);
-	}
-	histsize = linecap = 0; line = NULL;
-	while (getline(&line, &linecap, fp) > 0 && histsize < MAXHISTSIZE) {
-		if ((history[histsize++] = strdup(line)) == NULL)
-			ERROR(NULL, FATAL_SYSERR, false, "strdup()");
-		(void)strtok(history[histsize - 1], "\n\r");
-	}
-	free(line);
-	fclose(fp);
-
-	*size = histsize;
-
-	return (history);
+  _error = 0;
+  if ((pw = getpwuid(getuid())) == NULL)
+    ERROR(-1, FATAL_SYSERR, false, "getpwuid()");
+  (void)snprintf(histpath, sizeof(histpath), "%s/%s", pw->pw_dir, PATH_HISTORY);
+  (void)snprintf(tmpath, sizeof(tmpath), "%s/%s.XXXXX", pw->pw_dir,
+                 PATH_HISTORY);
+  endpwent();
+  if ((fd = mkstemp(tmpath)) == -1) ERROR(-1, FATAL_SYSERR, false, "mkstemp()");
+  if ((tmp = fdopen(fd, "w")) == NULL)
+    ERROR(-1, FATAL_SYSERR, false, "fdopen()");
+  for (i = 0; i < histsize; i++) (void)fprintf(tmp, "%s\n", history[i]);
+  (void)fclose(tmp);
+  if (rename(tmpath, histpath) == -1) {
+    set_error(FATAL_SYSERR, false, "rename(%s, %s)", tmpath, histpath);
+    (void)remove(tmpath);
+    return (-1);
+  }
+  return (0);
 }
 
-int
-dsbexec_add_to_history(const char *cmd)
-{
-	size_t i, n;
+static int strtoargv(const char *str, char **argv, size_t argvsz,
+                     size_t *argc) {
+  int squote, dquote, esc;
+  char *buf, *p;
+  size_t n;
 
-	_error = 0;
-	if (*cmd == '\0')
-		return (0);
-	if (histsize >= MAXHISTSIZE) {
-		histsize = MAXHISTSIZE;
-		free(history[histsize - 1]);
-		n = histsize - 1;
-	} else
-		n = histsize++;
-	for (i = n; i >= 1;  i--)
-		history[i] = history[i - 1];
-	if ((history[0] = strdup(cmd)) == NULL)
-		ERROR(-1, FATAL_SYSERR, false, "strdup()");
-	(void)strtok(history[0], "\n\r");
+  if ((p = buf = strdup(str)) == NULL)
+    ERROR(-1, FATAL_SYSERR, false, "strdup()");
+  while (isspace(*str)) str++;
+  squote = dquote = n = esc = 0;
+  for (; n < argvsz && *str != '\0'; str++) {
+    if (*str == '"') {
+      if (esc) {
+        *p++ = *str;
+        esc ^= 1;
+      } else if (squote)
+        *p++ = *str;
+      else
+        dquote ^= 1;
+    } else if (*str == '\'') {
+      if (esc) {
+        *p++ = *str;
+        esc ^= 1;
+      } else
+        squote ^= 1;
+    } else if (*str == '\\') {
+      if (squote) {
+        *p++ = *str;
+      } else if (esc) {
+        *p++ = *str;
+        esc ^= 1;
+      } else
+        esc ^= 1;
+    } else if (isspace(*str)) {
+      if (esc) {
+        *p++ = *str;
+        esc ^= 1;
+      } else if (dquote || squote) {
+        *p++ = *str;
+      } else {
+        *p = '\0';
+        if (dquote || squote) goto err_unterm;
+        while (isspace(str[1])) str++;
+        argv[n++] = strdup(buf);
+        if (argv[n - 1] == NULL) {
+          ERROR(-1, FATAL_SYSERR, false, "strdup()");
+        }
+        p = buf;
+      }
+    } else
+      *p++ = *str;
+  }
+  *p = '\0';
+  if (dquote || squote) goto err_unterm;
+  if (p != buf) {
+    argv[n++] = strdup(buf);
+    if (argv[n - 1] == NULL) ERROR(-1, FATAL_SYSERR, false, "strdup()");
+  }
+  free(buf);
+  *argc = n;
 
-	return (0);
-}
-
-int
-dsbexec_write_history()
-{
-	int	      fd;
-	char	      histpath[PATH_MAX], tmpath[PATH_MAX];
-	FILE	      *tmp;
-	size_t	      i;
-	struct passwd *pw;
-
-	_error = 0;
-	if ((pw = getpwuid(getuid())) == NULL)
-		ERROR(-1, FATAL_SYSERR, false, "getpwuid()");
-	(void)snprintf(histpath, sizeof(histpath), "%s/%s", pw->pw_dir,
-	    PATH_HISTORY);
-	(void)snprintf(tmpath, sizeof(tmpath), "%s/%s.XXXXX", pw->pw_dir,
-	    PATH_HISTORY);
-	endpwent();
-	if ((fd = mkstemp(tmpath)) == -1)
-		ERROR(-1, FATAL_SYSERR, false, "mkstemp()");
-	if ((tmp = fdopen(fd, "w")) == NULL)
-		ERROR(-1, FATAL_SYSERR, false, "fdopen()");
-	for (i = 0; i < histsize; i++)
-		(void)fprintf(tmp, "%s\n", history[i]);
-	(void)fclose(tmp);
-	if (rename(tmpath, histpath) == -1) {
-		set_error(FATAL_SYSERR, false, "rename(%s, %s)", tmpath,
-		    histpath);
-		(void)remove(tmpath);
-		return (-1);
-	}
-	return (0);
-}
-
-static int
-strtoargv(const char *str, char **argv, size_t argvsz, size_t *argc)
-{
-	int    squote, dquote, esc;
-	char   *buf, *p;
-	size_t n;
-
-	if ((p = buf = strdup(str)) == NULL)
-		ERROR(-1, FATAL_SYSERR, false, "strdup()");
-	while (isspace(*str))
-		str++;
-	squote = dquote = n = esc = 0;
-	for (; n < argvsz && *str != '\0'; str++) {
-		if (*str == '"') {
-			if (esc) {
-				*p++ = *str;
-				esc ^= 1;
-			} else if (squote)
-				*p++ = *str;
-			else
-				dquote ^= 1;
-		} else if (*str == '\'') {
-			if (esc) {
-				*p++ = *str;
-				esc ^= 1;
-			} else
-				squote ^= 1;
-		} else if (*str == '\\') {
-			if (squote) {
-				*p++ = *str;
-			} else if (esc) {
-				*p++ = *str;
-				esc ^= 1;
-			} else
-				esc ^= 1;
-		} else if (isspace(*str)) {
-			if (esc) {
-				*p++ = *str;
-				esc ^= 1;
-			} else if (dquote || squote) {
-				*p++ = *str;
-			} else {
-				*p = '\0';
-				if (dquote || squote)
-					goto err_unterm;
-				while (isspace(str[1]))
-					str++;
-				argv[n++] = strdup(buf);
-				if (argv[n - 1] == NULL) {
-					ERROR(-1, FATAL_SYSERR, false,
-					    "strdup()");
-				}
-				p = buf;
-			}
-		} else
-			*p++ = *str;
-	}
-	*p = '\0';
-	if (dquote || squote)
-		goto err_unterm;
-	if (p != buf) {
-		argv[n++] = strdup(buf);
-		if (argv[n - 1] == NULL)
-			ERROR(-1, FATAL_SYSERR, false, "strdup()");
-	}
-	free(buf);
-	*argc = n;
-
-	return (0);
+  return (0);
 err_unterm:
-	while (n-- > 0)
-		free(argv[n]);
-	free(buf);
-	ERROR(-1, DSBEXEC_EUNTERM, false, "Unterminated quoted string");
+  while (n-- > 0) free(argv[n]);
+  free(buf);
+  ERROR(-1, DSBEXEC_EUNTERM, false, "Unterminated quoted string");
 }
 
-static void
-set_error(int error, bool prepend, const char *fmt, ...)
-{
-	int	_errno;
-	char	errbuf[ERRBUFSZ];
-	size_t  len;
-	va_list ap;
+static void set_error(int error, bool prepend, const char *fmt, ...) {
+  int _errno;
+  char errbuf[ERRBUFSZ];
+  size_t len;
+  va_list ap;
 
-	_errno = errno;
-	_error = error;
-	va_start(ap, fmt);
-	if (prepend) {
-		if (error & DSBEXEC_ERR_FATAL) {
-			if (strncmp(errmsg, "Fatal: ", 7) == 0) {
-				(void)memmove(errmsg, errmsg + 7,
-				    strlen(errmsg) - 6);
-			}
-			(void)strlcpy(errbuf, "Fatal: ", sizeof(errbuf) - 1);
-			len = strlen(errbuf);
-		} else
-			len = 0;
-		(void)vsnprintf(errbuf + len, sizeof(errbuf) - len, fmt, ap);
-		len = strlen(errbuf);
-		(void)snprintf(errbuf + len, sizeof(errbuf) - len, ":%s",
-		    errmsg);
-		(void)strlcpy(errmsg, errbuf, sizeof(errmsg));
-	} else {
-		(void)vsnprintf(errmsg, sizeof(errmsg), fmt, ap);
-		if (error & DSBEXEC_ERR_FATAL) {
-			(void)snprintf(errbuf, sizeof(errbuf), "Fatal: %s",
-			    errmsg);
-			(void)strlcpy(errmsg, errbuf, sizeof(errmsg));
-		}
-	}
-	if ((error & DSBEXEC_ERR_SYS) && _errno != 0) {
-		_error += _errno;
-		len = strlen(errmsg);
-		(void)snprintf(errmsg + len, sizeof(errmsg) - len,
-		    ": %s", strerror(_errno));
-		errno = 0;
-	}
+  _errno = errno;
+  _error = error;
+  va_start(ap, fmt);
+  if (prepend) {
+    if (error & DSBEXEC_ERR_FATAL) {
+      if (strncmp(errmsg, "Fatal: ", 7) == 0) {
+        (void)memmove(errmsg, errmsg + 7, strlen(errmsg) - 6);
+      }
+      (void)strlcpy(errbuf, "Fatal: ", sizeof(errbuf) - 1);
+      len = strlen(errbuf);
+    } else
+      len = 0;
+    (void)vsnprintf(errbuf + len, sizeof(errbuf) - len, fmt, ap);
+    len = strlen(errbuf);
+    (void)snprintf(errbuf + len, sizeof(errbuf) - len, ":%s", errmsg);
+    (void)strlcpy(errmsg, errbuf, sizeof(errmsg));
+  } else {
+    (void)vsnprintf(errmsg, sizeof(errmsg), fmt, ap);
+    if (error & DSBEXEC_ERR_FATAL) {
+      (void)snprintf(errbuf, sizeof(errbuf), "Fatal: %s", errmsg);
+      (void)strlcpy(errmsg, errbuf, sizeof(errmsg));
+    }
+  }
+  if ((error & DSBEXEC_ERR_SYS) && _errno != 0) {
+    _error += _errno;
+    len = strlen(errmsg);
+    (void)snprintf(errmsg + len, sizeof(errmsg) - len, ": %s",
+                   strerror(_errno));
+    errno = 0;
+  }
 }
 
-bool
-dsbexec_running()
-{
-	int	       fifo;
-	struct passwd *pw;
+bool dsbexec_running() {
+  int fifo;
+  struct passwd *pw;
 
-	if ((pw = getpwuid(getuid())) == NULL)
-		err(EXIT_FAILURE, "getpwuid()");
-	endpwent();
-	(void)snprintf(path_fifo, sizeof(path_fifo), "%s/%s", pw->pw_dir,
-	    PATH_FIFO);
-	endpwent();
-	if (mkfifo(path_fifo, S_IWUSR | S_IRUSR) == -1) {
-		if (errno != EEXIST)
-			warn("mkfifo(%s)", path_fifo);
-	} else
-		(void)chmod(path_fifo, S_IRUSR | S_IWUSR);
-	if ((fifo = open(path_fifo, O_WRONLY | O_NONBLOCK)) != -1) {
-		/* Already running */
-		(void)close(fifo);
-		return (true);
-	}
-	if ((fifo = open(path_fifo, O_RDWR | O_NONBLOCK | O_CLOEXEC)) == -1)
-		warn("open(%s)", path_fifo);
-	return (false);
+  if ((pw = getpwuid(getuid())) == NULL) err(EXIT_FAILURE, "getpwuid()");
+  endpwent();
+  (void)snprintf(path_fifo, sizeof(path_fifo), "%s/%s", pw->pw_dir, PATH_FIFO);
+  endpwent();
+  if (mkfifo(path_fifo, S_IWUSR | S_IRUSR) == -1) {
+    if (errno != EEXIST) warn("mkfifo(%s)", path_fifo);
+  } else
+    (void)chmod(path_fifo, S_IRUSR | S_IWUSR);
+  if ((fifo = open(path_fifo, O_WRONLY | O_NONBLOCK)) != -1) {
+    /* Already running */
+    (void)close(fifo);
+    return (true);
+  }
+  if ((fifo = open(path_fifo, O_RDWR | O_NONBLOCK | O_CLOEXEC)) == -1)
+    warn("open(%s)", path_fifo);
+  return (false);
 }
-
--- src/mainwin.cpp
+++ src/mainwin.cpp
@@ -1,177 +1,146 @@
 /*-
- * Copyright (c) 2018 Marcel Kaiser. All rights reserved.
+ * Copyright (c) 2024 Marcel Kaiser. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
+ * SPDX-License-Identifier: BSD-2-Clause
  *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <QStringConverter>
+#include "mainwin.h"
+
 #include <QDir>
 #include <QScreen>
-#include "mainwin.h"
+#include <QStringConverter>
+
 #include "qt-helper/qt-helper.h"
 
 MainWin::MainWin(QWidget *parent) : QMainWindow(parent) {
-	edit	    	    = new QLineEdit(this);
-	QString prompt	    = QString(tr("Command:"));
-	QIcon pic	    = qh_loadIcon("system-run", NULL);
-	rootCb		    = new QCheckBox(tr("Execute as &root"));
-	statusMsg	    = new QLabel(this);
-	statusBar	    = new QStatusBar(this);
-	QLabel	    *icon   = new QLabel(this);	      
-	QLabel	    *label  = new QLabel(prompt);
-	QVBoxLayout *vbox   = new QVBoxLayout;
-	QVBoxLayout *evbox  = new QVBoxLayout;
-	QHBoxLayout *hbox   = new QHBoxLayout;
-	QWidget *container  = new QWidget(this);
+  edit = new QLineEdit(this);
+  QString prompt = QString(tr("Command:"));
+  QIcon pic = qh_loadIcon("system-run", NULL);
+  rootCb = new QCheckBox(tr("Execute as &root"));
+  statusMsg = new QLabel(this);
+  statusBar = new QStatusBar(this);
+  QLabel *icon = new QLabel(this);
+  QLabel *label = new QLabel(prompt);
+  QVBoxLayout *vbox = new QVBoxLayout;
+  QVBoxLayout *evbox = new QVBoxLayout;
+  QHBoxLayout *hbox = new QHBoxLayout;
+  QWidget *container = new QWidget(this);
 
-	icon->setPixmap(pic.pixmap(64));
-	setWindowIcon(pic);
-	label->setStyleSheet("font-weight: bold;");
+  icon->setPixmap(pic.pixmap(64));
+  setWindowIcon(pic);
+  label->setStyleSheet("font-weight: bold;");
 
-	hbox->addWidget(icon,   0, Qt::AlignLeft);
-	vbox->addLayout(hbox);
+  hbox->addWidget(icon, 0, Qt::AlignLeft);
+  vbox->addLayout(hbox);
 
-	evbox->addWidget(label, 1, Qt::AlignLeft);
-	evbox->addWidget(edit);
-	evbox->addWidget(statusMsg);
-	hbox->addLayout(evbox);
+  evbox->addWidget(label, 1, Qt::AlignLeft);
+  evbox->addWidget(edit);
+  evbox->addWidget(statusMsg);
+  hbox->addLayout(evbox);
 
-	vbox->addWidget(rootCb);
-	container->setLayout(vbox);
-	setCentralWidget(container);
+  vbox->addWidget(rootCb);
+  container->setLayout(vbox);
+  setCentralWidget(container);
 
-	setMinimumWidth(500);
-	setWindowFlags(windowFlags() | Qt::FramelessWindowHint);
-	show();
-	setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter,
-	    size(), qApp->primaryScreen()->geometry()));
-	connect(edit, SIGNAL(returnPressed()), this, SLOT(doExec()));
-	connect(edit, SIGNAL(textChanged(const QString &)), this,
-	    SLOT(resetStatusBar(const QString &)));
-	connect(rootCb, SIGNAL(clicked()), this, SLOT(rootCbClicked()));
-	initHistory();
-	initAutoCompleter();
+  setMinimumWidth(500);
+  setWindowFlags(windowFlags() | Qt::FramelessWindowHint);
+  show();
+  setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, size(),
+                                  qApp->primaryScreen()->geometry()));
+  connect(edit, SIGNAL(returnPressed()), this, SLOT(doExec()));
+  connect(edit, SIGNAL(textChanged(const QString &)), this,
+          SLOT(resetStatusBar(const QString &)));
+  connect(rootCb, SIGNAL(clicked()), this, SLOT(rootCbClicked()));
+  initHistory();
+  initAutoCompleter();
 }
 
-void
-MainWin::rootCbClicked()
-{
-	edit->setFocus();
+void MainWin::rootCbClicked() { edit->setFocus(); }
+
+void MainWin::resetStatusBar(const QString & /*unused*/) {
+  statusMsg->setText("");
 }
 
-void
-MainWin::resetStatusBar(const QString & /*unused*/)
-{
-	statusMsg->setText("");
+void MainWin::doExec() {
+  auto encoder{QStringEncoder(QStringEncoder::Utf8)};
+  QByteArray enccmd{encoder(edit->text())};
+
+  if (*enccmd.data() == '\0') return;
+  addToHistory(edit->text());
+  if (rootCb->checkState() == Qt::Checked) {
+    QByteArray encmsg{
+        encoder(tr("Execute command '%1' as root").arg(enccmd.data()))};
+    dsbexec_exec(true, enccmd.data(), encmsg.data());
+  } else
+    dsbexec_exec(false, enccmd.data(), NULL);
+  if ((dsbexec_error() & DSBEXEC_ENOENT))
+    statusMsg->setText(tr("Command not found"));
+  else if ((dsbexec_error() & DSBEXEC_EUNTERM))
+    statusMsg->setText(tr("Unterminated quoted string"));
+  else
+    qh_err(this, EXIT_FAILURE, "dsbexec_exec()");
 }
 
-void
-MainWin::doExec()
-{
-	auto encoder{QStringEncoder(QStringEncoder::Utf8)};
-    QByteArray enccmd{encoder(edit->text())};
-
-	if (*enccmd.data() == '\0')
-		return;
-	addToHistory(edit->text());
-	if (rootCb->checkState() == Qt::Checked) {
-		QByteArray encmsg{encoder(
-		    tr("Execute command '%1' as root").arg(enccmd.data()))};
-		dsbexec_exec(true, enccmd.data(), encmsg.data());
-	} else
-		dsbexec_exec(false, enccmd.data(), NULL);
-	if ((dsbexec_error() & DSBEXEC_ENOENT))
-		statusMsg->setText(tr("Command not found"));
-	else if ((dsbexec_error() & DSBEXEC_EUNTERM))
-		statusMsg->setText(tr("Unterminated quoted string"));
-	else
-		qh_err(this, EXIT_FAILURE, "dsbexec_exec()");
+void MainWin::closeEvent(QCloseEvent * /* unused */) {
+  QCoreApplication::exit(0);
 }
 
-void MainWin::closeEvent(QCloseEvent * /* unused */)
-{
-	QCoreApplication::exit(0);
-}
+void MainWin::initAutoCompleter() {
+  char *p, *paths;
+  QStringList list;
 
-void MainWin::initAutoCompleter()
-{
-	char	    *p, *paths;
-	QStringList list;
-
-	if ((p = getenv("PATH")) == NULL)
-		return;
-	if ((paths = strdup(p)) == NULL)
-		qh_err(this, EXIT_FAILURE, "strdup()");
-	for (p = paths; (p = strtok(p, ":")) != NULL; p = NULL) {
-		QDir dir(p);
-		list += dir.entryList(QStringList(), QDir::Files);
-		list.sort();
-	}
-	free(paths);
-	QCompleter *completer = new QCompleter(list, this);
-	completer->setCaseSensitivity(Qt::CaseSensitive);
-	completer->setCompletionMode(QCompleter::PopupCompletion);
-	edit->setCompleter(completer);
+  if ((p = getenv("PATH")) == NULL) return;
+  if ((paths = strdup(p)) == NULL) qh_err(this, EXIT_FAILURE, "strdup()");
+  for (p = paths; (p = strtok(p, ":")) != NULL; p = NULL) {
+    QDir dir(p);
+    list += dir.entryList(QStringList(), QDir::Files);
+    list.sort();
+  }
+  free(paths);
+  QCompleter *completer = new QCompleter(list, this);
+  completer->setCaseSensitivity(Qt::CaseSensitive);
+  completer->setCompletionMode(QCompleter::PopupCompletion);
+  edit->setCompleter(completer);
 }
 
 void MainWin::keyPressEvent(QKeyEvent *e) {
-	if (e->key() == Qt::Key_Escape)
-		QCoreApplication::exit(0);
-	else if (e->key() == Qt::Key_Up) {
-		if (histCursor < history->count() - 1)
-			edit->setText(history->at(++histCursor));
-		e->ignore();
-	} else if (e->key() == Qt::Key_Down) {
-		if (histCursor > 0) {
-			edit->setText(history->at(--histCursor));
-		} else {
-			histCursor = -1;
-			edit->setText("");
-		}
-		e->ignore();
-	}
+  if (e->key() == Qt::Key_Escape)
+    QCoreApplication::exit(0);
+  else if (e->key() == Qt::Key_Up) {
+    if (histCursor < history->count() - 1)
+      edit->setText(history->at(++histCursor));
+    e->ignore();
+  } else if (e->key() == Qt::Key_Down) {
+    if (histCursor > 0) {
+      edit->setText(history->at(--histCursor));
+    } else {
+      histCursor = -1;
+      edit->setText("");
+    }
+    e->ignore();
+  }
 }
 
-void MainWin::initHistory()
-{
-	char   **hv;
-	size_t size;
+void MainWin::initHistory() {
+  char **hv;
+  size_t size;
 
-	histCursor = -1;
-	history = new QStringList;
+  histCursor = -1;
+  history = new QStringList;
 
-	if ((hv = dsbexec_read_history(&size)) == NULL) {
-		if (dsbexec_error() != 0)
-			qh_errx(NULL, EXIT_FAILURE, "%s", dsbexec_strerror());
-		return;
-	}
-	for (size_t i = 0; i < size; i++) {
-		QString s(hv[i]);
-		history->append(s);
-	}
+  if ((hv = dsbexec_read_history(&size)) == NULL) {
+    if (dsbexec_error() != 0)
+      qh_errx(NULL, EXIT_FAILURE, "%s", dsbexec_strerror());
+    return;
+  }
+  for (size_t i = 0; i < size; i++) {
+    QString s(hv[i]);
+    history->append(s);
+  }
 }
 
-void MainWin::addToHistory(QString s)
-{
-	history->prepend(s);
-	histCursor = -1;
+void MainWin::addToHistory(QString s) {
+  history->prepend(s);
+  histCursor = -1;
 }
-
